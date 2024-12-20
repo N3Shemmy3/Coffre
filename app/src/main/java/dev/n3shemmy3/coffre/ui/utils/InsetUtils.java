68d6b2c016f6d81bbdc84c0d2f08272b2899777d
Shemmy <shemmynyirenda3@gmail.com>
2024-12-20 04:25:46 +0200

    fixed applySystemBarsMargin

--- app/src/main/java/dev/n3shemmy3/coffre/ui/utils/InsetUtils.java
+++ app/src/main/java/dev/n3shemmy3/coffre/ui/utils/InsetUtils.java
@@ -3,6 +3,7 @@ package dev.n3shemmy3.coffre.ui.utils;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,7 +13,7 @@ import dev.n3shemmy3.coffre.R;
 public class InsetUtils {
 
 
-    public static void applySystemBarsInsets(View view, boolean left, boolean top, boolean right, boolean bottom) {
+    public static void applySystemBarsInsets(@NonNull View view, boolean left, boolean top, boolean right, boolean bottom) {
         ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.topAppBar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(
@@ -25,17 +26,18 @@ public class InsetUtils {
         });
     }
 
-    public static void applySystemBarsMargin(View view, boolean left, boolean top, boolean right, boolean bottom) {
-        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-        ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.topAppBar), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            marginLayoutParams.setMargins(
-                    left ? systemBars.left : marginLayoutParams.getMarginStart(),
-                    top ? systemBars.top : marginLayoutParams.topMargin,
-                    right ? systemBars.right : marginLayoutParams.getMarginEnd(),
-                    bottom ? v.getPaddingBottom() : marginLayoutParams.bottomMargin);
-            view.setLayoutParams(marginLayoutParams);
-            return insets;
+    public static void applySystemBarsMargin(@NonNull View view, boolean left, boolean top, boolean right, boolean bottom) {
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.leftMargin = left ? insets.left : mlp.leftMargin;
+            mlp.topMargin = top ? insets.top : mlp.topMargin;
+            mlp.rightMargin = right ? insets.right : mlp.rightMargin;
+            mlp.bottomMargin = bottom ? insets.bottom : mlp.bottomMargin;
+            v.setLayoutParams(mlp);
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            return WindowInsetsCompat.CONSUMED;
         });
     }
 }
