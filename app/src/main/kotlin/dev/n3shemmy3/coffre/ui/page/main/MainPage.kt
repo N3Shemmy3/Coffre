eadca447d0185cba2fc871d3a2aac94ae1a76c40
Shemmy <shemmynyirenda3@gmail.com>
2025-01-13 07:20:40 +0200

    added a balance card

--- app/src/main/kotlin/dev/n3shemmy3/coffre/ui/page/main/MainPage.kt
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/page/main/MainPage.kt
@@ -2,11 +2,11 @@ package dev.n3shemmy3.coffre.ui.page.main
 
 import android.annotation.SuppressLint
 import android.view.HapticFeedbackConstants
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.DateRange
@@ -24,18 +25,23 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import dev.n3shemmy3.coffre.R
@@ -51,40 +57,34 @@ import dev.n3shemmy3.coffre.ui.theme.Spacing_content_horizontal
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage(navController: NavHostController) {
-    AppScaffold(
-        useLargeAppBar = false,
-        title = "",
-        navigationIcon = {
-            FeedBackIcon(R.drawable.avatar, null, onClick = {
-                navController.navigate(RouteName.PROFILE)
-            })
-        },
-        actions = {
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Outlined.Search, contentDescription = "Search",
-                )
-            }
-            IconButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Outlined.DateRange, contentDescription = "Calender"
-                )
-            }
-        },
-        content = { paddingValues, hInsets ->
-            val numbers = remember { mutableListOf(1, 2, 3, 4, 5, 6, 7) }
-            TransactionList(
-                paddingValues = paddingValues, hInsets = hInsets,
-                numbers = numbers
-            )
-        }, floatingActionButton = {
-            val view = LocalView.current
-            FloatingActionButton(modifier = Modifier.size(64.dp), shape = Shape20, onClick = {
-                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-            }) {
-                Icon(Icons.Outlined.Add, null)
-            }
+    AppScaffold(useLargeAppBar = false, title = "", navigationIcon = {
+        FeedBackIcon(R.drawable.avatar, null, onClick = {
+            navController.navigate(RouteName.PROFILE)
         })
+    }, actions = {
+        IconButton(onClick = {}) {
+            Icon(
+                imageVector = Icons.Outlined.Search, contentDescription = "Search",
+            )
+        }
+        IconButton(onClick = {}) {
+            Icon(
+                imageVector = Icons.Outlined.DateRange, contentDescription = "Calender"
+            )
+        }
+    }, content = { paddingValues, hInsets ->
+        val numbers = remember { mutableListOf(1, 2, 3, 4, 5, 6, 7) }
+        TransactionList(
+            paddingValues = paddingValues, hInsets = hInsets, numbers = numbers
+        )
+    }, floatingActionButton = {
+        val view = LocalView.current
+        FloatingActionButton(modifier = Modifier.size(64.dp), shape = Shape20, onClick = {
+            view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+        }) {
+            Icon(Icons.Outlined.Add, null)
+        }
+    })
 }
 
 @Composable
@@ -101,21 +101,105 @@ fun TransactionList(paddingValues: PaddingValues, hInsets: Dp, numbers: MutableL
             Surface(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(height = 230.dp, width = 0.dp)
+                    .defaultMinSize(minHeight = 250.dp, minWidth = 0.dp)
                     .absolutePadding(
-                        top = Spacing_content_horizontal,
-                        bottom = Spacing_content_horizontal
-                    ),
-                tonalElevation = 4.dp,
-                shape = MaterialTheme.shapes.extraLarge
+                        top = Spacing_content_horizontal, bottom = Spacing_content_horizontal
+                    ), tonalElevation = 4.dp, shape = MaterialTheme.shapes.extraLarge
 
             ) {
-                Column(
+                ConstraintLayout(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(all = Spacing_content_horizontal)
+                        .padding(all = 20.dp)
                 ) {
-                    Text(text = "Test")
+                    val (accountName, balanceCurrency, balanceRound, balanceDecimal, incomeTitle, incomeValue, expenseTitle, expenseValue, balanceBar) = createRefs()
+                    Text(
+                        text = "Expenses",
+                        fontWeight = FontWeight.W600,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .constrainAs(accountName) {
+                                start.linkTo(parent.start)
+                                top.linkTo(parent.top)
+                            })
+                    Text(
+                        text = "$",
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier
+                            .alpha(.72f)
+                            .constrainAs(balanceCurrency) {
+                                start.linkTo(accountName.start)
+                                top.linkTo(balanceDecimal.top)
+                                bottom.linkTo(balanceDecimal.bottom)
+                            })
+                    Text(
+                        text = "- 123.",
+                        style = MaterialTheme.typography.displayLarge,
+                        modifier = Modifier
+                            .constrainAs(balanceRound) {
+                                top.linkTo(accountName.top, 24.dp)
+                                end.linkTo(balanceDecimal.start, 2.dp)
+                            })
+                    Text(
+                        text = "45",
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier
+                            .constrainAs(balanceDecimal) {
+                                end.linkTo(parent.end)
+                                bottom.linkTo(balanceRound.bottom, 4.dp)
+                            })
+                    Text(
+                        text = "Income",
+                        fontWeight = FontWeight.W600,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .alpha(.72f)
+                            .constrainAs(incomeTitle) {
+                                start.linkTo(accountName.start)
+                                top.linkTo(balanceRound.bottom, margin = 16.dp)
+                            })
+                    Text(
+                        text = "+ $4340",
+                        fontWeight = FontWeight.W700,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .alpha(.72f)
+                            .constrainAs(incomeValue) {
+                                top.linkTo(incomeTitle.top)
+                                end.linkTo(balanceDecimal.end)
+                                bottom.linkTo(incomeTitle.bottom)
+                            })
+                    Text(
+                        text = "Expenses",
+                        fontWeight = FontWeight.W600,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .alpha(.72f)
+                            .constrainAs(expenseTitle) {
+                                start.linkTo(accountName.start)
+                                top.linkTo(incomeTitle.bottom, margin = 12.dp)
+                            })
+                    Text(
+                        text = "- $2340",
+                        fontWeight = FontWeight.W700,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .alpha(.72f)
+                            .constrainAs(expenseValue) {
+                                top.linkTo(expenseTitle.top)
+                                end.linkTo(balanceDecimal.end)
+                                bottom.linkTo(expenseTitle.bottom)
+                            })
+                    LinearProgressIndicator(
+                        progress = { .7f },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(CircleShape)
+                            .constrainAs(balanceBar) {
+                                start.linkTo(expenseTitle.start)
+                                top.linkTo(expenseTitle.bottom, margin = 16.dp)
+                            },
+                    )
                 }
             }
         }
