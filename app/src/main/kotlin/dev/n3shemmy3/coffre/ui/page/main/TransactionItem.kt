032b28750356f3e07b1bda2179fdfba7d197e8ee
Shemmy <shemmynyirenda3@gmail.com>
2025-01-13 09:33:14 +0200

    created item

--- app/src/main/kotlin/dev/n3shemmy3/coffre/ui/page/main/TransactionItem.kt
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/page/main/TransactionItem.kt
@@ -2,23 +2,19 @@ package dev.n3shemmy3.coffre.ui.page.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -26,6 +22,7 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import dev.n3shemmy3.coffre.ui.theme.CoffreTheme
 import dev.n3shemmy3.coffre.ui.theme.Spacing_content_horizontal
 import dev.n3shemmy3.coffre.ui.theme.Spacing_content_vertical
@@ -42,14 +39,16 @@ fun TransactionItem(index: Int, shape: Shape) {
             .fillMaxWidth()
             .clickable { },
     ) {
-        Row(
+        ConstraintLayout(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(
                     horizontal = Spacing_content_horizontal, vertical = Spacing_content_horizontal
                 ),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
+
+            ) {
+            val (itemIcon, itemTitle, itemSummary, itemAction) = createRefs()
+
             Icon(
                 modifier = Modifier
                     .background(
@@ -57,18 +56,51 @@ fun TransactionItem(index: Int, shape: Shape) {
                         shape = CircleShape
                     )
                     .padding(all = Spacing_content_vertical)
-                    .clip(CircleShape),
+                    .clip(CircleShape)
+                    .constrainAs(itemIcon) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    },
                 tint = MaterialTheme.colorScheme.onSurface,
                 imageVector = Icons.Outlined.Edit,
                 contentDescription = null
             )
-            Spacer(
-                modifier = Modifier
-                    .size(width = Spacing_content_horizontal, height = 0.dp)
+
+
+            Text(
+                text = "Item Title",
+                modifier = Modifier.constrainAs(itemTitle) {
+                    start.linkTo(itemIcon.end, Spacing_content_horizontal)
+                    top.linkTo(itemIcon.top)
+                },
+                style = MaterialTheme.typography.bodyLarge
             )
-            Column(modifier = Modifier.absolutePadding(right = Spacing_content_horizontal)) {
-                Text(text = "Item Title", style = MaterialTheme.typography.bodyLarge)
-                Text(text = "Item subTitle", style = MaterialTheme.typography.bodyMedium, modifier = Modifier.alpha(.6f))
+            Text(
+                text = "Item subTitle",
+                modifier = Modifier
+                    .alpha(.6f)
+                    .constrainAs(itemSummary) {
+                        start.linkTo(itemTitle.start)
+                        top.linkTo(itemTitle.bottom)
+                    },
+                style = MaterialTheme.typography.bodyMedium,
+            )
+            Box(
+                modifier = Modifier
+                    .background(
+                        MaterialTheme.colorScheme.errorContainer,
+                        shape = CircleShape
+                    )
+                    .padding(all = Spacing_content_vertical)
+                    .clip(CircleShape)
+                    .constrainAs(itemAction) {
+                        end.linkTo(parent.end)
+                        top.linkTo(itemTitle.top)
+                        bottom.linkTo(itemIcon.bottom)
+                    }
+            ) {
+                Text(text = "-$50", style = MaterialTheme.typography.labelSmall, color = MaterialTheme.colorScheme.onErrorContainer)
             }
 
         }
