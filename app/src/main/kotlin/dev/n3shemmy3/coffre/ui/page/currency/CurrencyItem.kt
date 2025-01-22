d5bccf6ec8fef62453c21d782d89f5605a698169
Shemmy <shemmynyirenda3@gmail.com>
2025-01-22 21:38:03 +0200

    Add Gson and DotsIndicator dependencies
    Added the Gson and DotsIndicator libraries as dependencies to the project.

---
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/page/currency/CurrencyItem.kt
@@ -0,0 +1,139 @@
+package dev.n3shemmy3.coffre.ui.page.currency
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import dev.n3shemmy3.coffre.domain.currency.Currency
+import dev.n3shemmy3.coffre.ui.theme.Spacing_content_horizontal
+import dev.n3shemmy3.coffre.ui.theme.Spacing_content_vertical
+import dev.n3shemmy3.coffre.ui.theme.Spacing_page_horizontal
+
+@OptIn(
+    ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class,
+)
+@Composable
+fun CurrencyItem(
+    currency: Currency,
+    isSelected: Boolean,
+    onClick: () -> Unit = {},
+) {
+    var layoutDirection = LocalLayoutDirection.current
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(true) {
+                onClick()
+            }
+            .background(MaterialTheme.colorScheme.background)
+            .padding(
+                horizontal = WindowInsets.displayCutout.asPaddingValues()
+                    .calculateStartPadding(layoutDirection) + Spacing_page_horizontal,
+                vertical = Spacing_content_horizontal
+            ),
+
+        ) {
+        val (itemIcon, itemContent, itemAction) = createRefs()
+        Box(
+            modifier = Modifier
+                .constrainAs(itemIcon) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                }
+                .background(
+                    MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp), shape = CircleShape
+                )
+                .size(48.dp)
+                .clip(CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(
+                text = currency.symbol,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+        Column(modifier = Modifier.constrainAs(itemContent) {
+            start.linkTo(itemIcon.end)
+            top.linkTo(itemIcon.top)
+            end.linkTo(itemAction.start)
+        }) {
+
+            Text(
+                text = currency.name,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = currency.code,
+                modifier = Modifier
+                    .alpha(.6f),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Start
+            )
+        }
+        AnimatedVisibility(
+            visible = isSelected,
+            modifier = Modifier
+                .constrainAs(itemAction) {
+                    end.linkTo(parent.end)
+                    top.linkTo(itemContent.top)
+                    bottom.linkTo(itemIcon.bottom)
+                }) {
+            Box(
+                modifier = Modifier
+                    .background(
+                        MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp), shape = CircleShape
+                    )
+                    .padding(all = Spacing_content_vertical)
+                    .clip(CircleShape)
+
+            ) {
+                Icon(
+                    Icons.Outlined.Check,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CurrencyItemPreview() {
+    CurrencyItem(
+        currency = Currency("AED", "United Arab Emirates Dirham", "د.إ", "د.إ"),
+        isSelected = true,
+        onClick = {})
+}
---
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/page/currency/CurrencyPage.kt
@@ -0,0 +1,114 @@
+package dev.n3shemmy3.coffre.ui.page.currency
+
+import ReadCurrencyData
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import dev.n3shemmy3.coffre.domain.currency.Currency
+import dev.n3shemmy3.coffre.ui.component.base.AppScaffold
+import dev.n3shemmy3.coffre.ui.component.base.FeedBackIcon
+import dev.n3shemmy3.coffre.ui.theme.Spacing_content_vertical
+
+@Composable
+fun CurrencyPage(navController: NavController) {
+    var currencyList by remember { mutableStateOf<List<Currency>?>(null) }
+    val scrollState = rememberLazyListState()
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        currencyList = ReadCurrencyData(context)
+    }
+    AppScaffold(
+        useLargeAppBar = true, navigationIcon = {
+            FeedBackIcon(
+                Icons.AutoMirrored.Outlined.ArrowBack,
+                null,
+                onClick = { navController.popBackStack() })
+        }, title = "Currency",
+        content = { paddingValues ->
+            var layoutDirection = LocalLayoutDirection.current
+            val paddingValues = PaddingValues(
+                start = paddingValues.calculateLeftPadding(layoutDirection) + Spacing_content_vertical,
+                top = paddingValues.calculateTopPadding(),
+                end = paddingValues.calculateEndPadding(layoutDirection) + Spacing_content_vertical,
+                bottom = 0.dp
+            )
+            LazyColumn(
+                state = scrollState,
+                modifier = Modifier
+                    .fillMaxSize()
+
+            ) {
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                top =
+                                    paddingValues.calculateTopPadding(),
+                                start = paddingValues.calculateLeftPadding(layoutDirection),
+                                end = paddingValues.calculateLeftPadding(layoutDirection),
+
+                                )
+                    ) {
+                        Text(
+                            text = "Set your currency",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                }
+                currencyList?.sortedBy { it.name }?.forEach { currency ->
+                    item(key = currency.hashCode()) {
+                        var isSelected by remember { mutableStateOf(false) }
+                        CurrencyItem(
+                            currency = currency,
+                            isSelected = isSelected,
+                            onClick = { isSelected = !isSelected })
+                    }
+                }
+                item {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(paddingValues.calculateBottomPadding())
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun CurrencyPagePreview() {
+    val navController = rememberNavController()
+    CurrencyPage(navController)
+}
