bfa95700f05450bebdd996931296b1eb58e4e22f
Shemmy <shemmynyirenda3@gmail.com>
2025-02-27 15:14:40 +0200

    enable dynamic colors

---
+++ app/src/main/java/dev/n3shemmy3/coffre/ui/App.java
@@ -0,0 +1,13 @@
+package dev.n3shemmy3.coffre.ui;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
