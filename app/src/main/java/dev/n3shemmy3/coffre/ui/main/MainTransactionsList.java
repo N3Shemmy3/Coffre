bb1caff2853a510e5de9e3c7dbd17fea24e8bcbf
Shemmy <shemmynyirenda3@gmail.com>
2025-02-27 19:39:31 +0200

    prettify amounts

--- app/src/main/java/dev/n3shemmy3/coffre/ui/main/MainTransactionsList.java
+++ app/src/main/java/dev/n3shemmy3/coffre/ui/main/MainTransactionsList.java
@@ -51,7 +51,7 @@ public class MainTransactionsList extends BaseFragment {
         transaction.setId(transactions.size() - 1);
         transaction.setTitle("Salary");
         transaction.setDescription("Income");
-        transaction.setAmount(200);
+        transaction.setAmount(20.00);
         transaction.setTransactionType(Transaction.TransactionType.INCOME);
         transactions.add(transaction);
 
@@ -60,7 +60,7 @@ public class MainTransactionsList extends BaseFragment {
         transaction.setId(transactions.size() - 1);
         transaction.setTitle("Bank to Cash");
         transaction.setDescription("Transfer");
-        transaction.setAmount(100);
+        transaction.setAmount(10.00);
         transaction.setTransactionType(Transaction.TransactionType.TRANSFER);
         transactions.add(transaction);
         adapter.submitList(transactions);
