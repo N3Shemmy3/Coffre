f09f91b1e352aafaa0ce859a08a7e54ab2090b78
Shemmy <shemmynyirenda3@gmail.com>
2025-02-27 15:06:29 +0200

    Project Creation

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "dev.n3shemmy3.coffre"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "dev.n3shemmy3.coffre"
+        minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildToolsVersion = "35.0.0"
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.coordinatorlayout)
+    implementation(libs.fragment)
+    implementation(libs.glide)
+    implementation(libs.eventbus)
+    implementation(libs.viewpager2)
+    implementation(libs.viewmodel)
+    implementation(libs.viewmodel.savedstate)
+    implementation(libs.livedata)
+    implementation(libs.splashscreen)
+    implementation(libs.recyclerview)
+    implementation(libs.recyclerview.selection)
+    implementation(libs.preference)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
