dc4dd179cc4fcbbe3b60fbee6eb1c7e40425c2e7
Shemmy <shemmynyirenda3@gmail.com>
2025-03-04 03:23:48 +0200

    Initialize Iconics library

--- app/src/main/java/dev/n3shemmy3/coffre/ui/App.java
+++ app/src/main/java/dev/n3shemmy3/coffre/ui/App.java
@@ -3,11 +3,13 @@ package dev.n3shemmy3.coffre.ui;
 import android.app.Application;
 
 import com.google.android.material.color.DynamicColors;
+import com.mikepenz.iconics.Iconics;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         DynamicColors.applyToActivitiesIfAvailable(this);
+        Iconics.init(getApplicationContext());
     }
 }
