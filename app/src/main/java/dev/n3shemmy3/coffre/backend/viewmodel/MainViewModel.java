a1df71f70ffdec4b54e930ddd9d044c5e5225a48
Shemmy <shemmynyirenda3@gmail.com>
2025-03-27 00:40:20 +0200

    feat: Add balance calculation and display in main screen
    This commit introduces the following changes:
    
    - Adds calculation of net balance, total income, and total expenses in `MainViewModel` using `BigDecimal` for accurate financial calculations.
    - Implements live updates of these calculated values in the `MainBalanceCard` fragment.
    - Formats and displays the balance using `TickerView` for a visually appealing number animation.
    - Includes helper functions for formatting and extracting decimal parts from `BigDecimal` values.

--- app/src/main/java/dev/n3shemmy3/coffre/backend/viewmodel/MainViewModel.java
+++ app/src/main/java/dev/n3shemmy3/coffre/backend/viewmodel/MainViewModel.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import dev.n3shemmy3.coffre.backend.item.Transaction;
@@ -15,12 +16,18 @@ public class MainViewModel extends AndroidViewModel {
 
     private final TransactionRepository repository;
     private final LiveData<List<Transaction>> transactions;
+    private final LiveData<BigDecimal> netBalance;
+    private final LiveData<BigDecimal> totalExpenses;
+    private final LiveData<BigDecimal> totalIncome;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
 
         repository = new TransactionRepository(application);
         transactions = repository.getAllTransactions();
+        netBalance = repository.getNetBalance();
+        totalIncome = repository.getTotalIncome();
+        totalExpenses = repository.getTotalExpenses();
     }
 
     public void insert(Transaction item) {
@@ -41,5 +48,17 @@ public class MainViewModel extends AndroidViewModel {
 
     public LiveData<List<Transaction>> getTransactions() {
         return this.transactions;
+    }
+
+    public LiveData<BigDecimal> getNetBalance() {
+        return netBalance;
+    }
+
+    public LiveData<BigDecimal> getTotalExpenses() {
+        return totalExpenses;
+    }
+
+    public LiveData<BigDecimal> getTotalIncome() {
+        return totalIncome;
     }
 }
--- app/src/main/java/dev/n3shemmy3/coffre/ui/screen/main/MainBalanceCard.java
+++ app/src/main/java/dev/n3shemmy3/coffre/ui/screen/main/MainBalanceCard.java
@@ -2,14 +2,30 @@ package dev.n3shemmy3.coffre.ui.screen.main;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import dev.n3shemmy3.coffre.R;
+import dev.n3shemmy3.coffre.backend.viewmodel.MainViewModel;
 import dev.n3shemmy3.coffre.ui.base.BaseFragment;
 
 public class MainBalanceCard extends BaseFragment {
+    private MainViewModel viewModel;
+
+    private TickerView round;
+    private TickerView decimal;
+    private TickerView income;
+    private TickerView expenses;
+
     @Override
     protected int getLayoutResId() {
         return R.layout.fragment_main_balance_card;
@@ -17,8 +33,41 @@ public class MainBalanceCard extends BaseFragment {
 
     @Override
     public void onCreateFragment(@NonNull View root, @Nullable Bundle savedInstanceState) {
-
+        round = root.findViewById(R.id.round);
+        decimal = root.findViewById(R.id.decimal);
+        income = root.findViewById(R.id.income);
+        expenses = root.findViewById(R.id.expenses);
+        round.setCharacterLists(TickerUtils.provideNumberList());
+        decimal.setCharacterLists(TickerUtils.provideNumberList());
+        income.setCharacterLists(TickerUtils.provideNumberList());
+        expenses.setCharacterLists(TickerUtils.provideNumberList());
     }
 
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+        viewModel.getNetBalance().observe(getViewLifecycleOwner(), netBalance -> {
+            BigDecimal formattedBalance = formatAmount(netBalance);
+            BigDecimal intPart = new BigDecimal(formattedBalance.toBigInteger());
+            round.setText(String.valueOf(intPart.intValue()));
+            decimal.setText(String.valueOf(getAbsDecimalPart(formattedBalance)).replaceFirst("^0", ""));
+        });
+        viewModel.getTotalIncome().observe(getViewLifecycleOwner(), incomeBalance -> income.setText("$" + formatAmount(incomeBalance)));
+        viewModel.getTotalExpenses().observe(getViewLifecycleOwner(), expenseBalance -> expenses.setText("$" + formatAmount(expenseBalance)));
+
+    }
+
+    public BigDecimal getAbsDecimalPart(BigDecimal bigDecimal) {
+        BigDecimal absoluteBigDecimal = bigDecimal.abs();
+        BigDecimal intPart = new BigDecimal(absoluteBigDecimal.toBigInteger());
+        BigDecimal decimalPart = absoluteBigDecimal.subtract(intPart);
+        return decimalPart.setScale(decimalPart.scale(), RoundingMode.DOWN);
+    }
+
+    private BigDecimal formatAmount(BigDecimal balance) {
+        return String.valueOf(balance).equals("null") ? BigDecimal.valueOf(0.00) : balance.setScale(2, RoundingMode.DOWN);
+    }
 }
