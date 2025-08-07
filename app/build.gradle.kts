e649ea0d348204b12d9d3809226076807cd6ee79
Shemmy <shemmynyirenda3@gmail.com>
2025-08-07 09:41:45 +0200

    add:material icons extended dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
+    implementation(libs.compose.icons.extended)
 
     /* Chart Library */
     implementation(libs.vico.compose)
