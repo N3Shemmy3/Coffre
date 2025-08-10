c2eb2bc3d0670ebc5e0782678cb082d8849d21da
Shemmy <shemmynyirenda3@gmail.com>
2025-08-10 17:48:06 +0200

    feat:account class

---
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/data/entity/Account.kt
@@ -0,0 +1,28 @@
+/*
+ *
+ *  * Copyright (C) 2025 Shemmy
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, version 3 of the License.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package dev.n3shemmy3.coffre.data.entity
+
+data class Account(
+    val id: Long,
+    val name: String,
+    val icon: String? = null,
+    val number: String? = null,
+    val memo: String? = null,
+    val hidden: Boolean = false  // true means exclude from summaries/reports
+)
