b5b4eb76615a77d957e9d91f3755cb08535d8ba0
Shemmy <shemmynyirenda3@gmail.com>
2025-09-04 11:10:07 +0200

    use fill color for md3 expressive

--- app/src/main/kotlin/dev/n3shemmy3/coffre/ui/component/ActionIconButton.kt
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/component/ActionIconButton.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -73,7 +74,7 @@ fun ActionIconButton(onClick: () -> Unit, imageVector: ImageVector, contentDescr
             }
         },
     ) {
-        IconButton(
+        FilledTonalIconButton(
             onClick = onClick,
         ) {
             Icon(
