6a9d5c777faaa25a1396dfb5f7156754e37ffc41
n3shemmy3 <67279072+N3Shemmy3@users.noreply.github.com>
2026-02-18 00:01:25 +0200

    created leadingIcon to slot into listitem

---
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/common/LeadingIcon.kt
@@ -0,0 +1,34 @@
+package dev.n3shemmy3.coffre.ui.common
+
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CreditCard
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LeadingIcon(
+    imageVector: ImageVector,
+    contentDescription: String?,
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier.defaultMinSize(48.dp, 48.dp),
+    colors: IconButtonColors = IconButtonDefaults.filledTonalIconButtonColors(
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+    ),
+) {
+    FilledTonalIconButton(
+        onClick = onClick,
+        modifier,
+        colors = colors,
+    ) {
+        Icon(imageVector, contentDescription)
+    }
+}
