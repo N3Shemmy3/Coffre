e5520cbe12fbc3a5f98937a22a023e420c4e30ac
n3shemmy3 <67279072+N3Shemmy3@users.noreply.github.com>
2026-02-18 13:27:23 +0200

    feedback file

---
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/ui/common/HaptickFeedback.kt
@@ -0,0 +1,11 @@
+package dev.n3shemmy3.coffre.ui.common
+
+import android.view.HapticFeedbackConstants
+import android.view.View
+
+object HapticFeedback {
+    fun View.shortPressHapticFeedback() = this.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK)
+
+    fun View.longPressHapticFeedback() =
+        this.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
+}
