ba936b3daeea90034abfef2017b129489c56b08b
n3shemmy3 <67279072+N3Shemmy3@users.noreply.github.com>
2026-02-26 10:15:43 +0200

    kinda testing navigation so finish affinity

--- app/src/main/kotlin/dev/n3shemmy3/coffre/CrashReportActivity.kt
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/CrashReportActivity.kt
@@ -6,8 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import dev.n3shemmy3.coffre.ui.screen.crashreport.CrashReportScreen
-import dev.n3shemmy3.coffre.ui.theme.AppTheme
+import dev.n3shemmy3.coffre.compose.screen.crashreport.CrashReportScreen
+import dev.n3shemmy3.coffre.compose.theme.AppTheme
 
 class CrashReportActivity : AppCompatActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -29,6 +29,6 @@ class CrashReportActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if (isFinishing) finishAffinity()
+        //if (isFinishing) finishAffinity()
     }
 }
