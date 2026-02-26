00bd9cada0f62b5b7192d7b7c611cbf3a28708dd
n3shemmy3 <67279072+N3Shemmy3@users.noreply.github.com>
2026-02-26 10:08:19 +0200

    added entity annotation and isPublic var

--- app/src/main/kotlin/dev/n3shemmy3/coffre/domain/model/Account.kt
+++ app/src/main/kotlin/dev/n3shemmy3/coffre/domain/model/Account.kt
@@ -1,16 +1,20 @@
 package dev.n3shemmy3.coffre.domain.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.math.BigDecimal
 
 
+@Entity("Accounts")
 data class Account(
     @PrimaryKey(autoGenerate = true) val id: Long,
     val name: String,
     val note: String?,
     val time: Long,
     val type: Account.Type,
-    val balance: BigDecimal
+    val balance: BigDecimal,
+    // isPublic: indicates whether the balance is added to total user balance in UIs
+    val isPublic: Boolean
 ) {
     enum class Type {
         Cash,
